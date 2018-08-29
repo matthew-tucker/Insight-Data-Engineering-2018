e6152854c9054a2e2c159499756c993a80c3f55b
Matt Tucker <matt.tucker@nyu.edu>
2018-08-29 11:12:10 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# Program Description
+
+This program compares stock values predicted by a provided model to actual stock values observed over the same time frame and calcuates the average error over a specified sliding time window. The error for a given stock's prediction is defined as: `error = | actual_value - predicted_value|` and the program finds the mean of the error for a given time window.
+
+Given an input file with stock values for a time interval `(1, ..., n)` and a window size `x`, this program finds the average error for all the windows formed by sliding a window of size `x` from `1` to `n-x`, with all intervals inclusive (thus, `(1, 2, 3, 4)` with a window size of four includes both `1` and `4`.)
+
+# Usage
+
+For directory structures that match the Insight challenge prompt requirements, the program can be run by executing `run.sh` in the root directory.
+
+## Input
+
+The input should contain three files, all stored in `input/`:
+
+* `actual.txt`: the actual observed stock values
+* `predicted.txt`: the model-predicted stock values
+* `window.txt`: the size of the sliding window
+
+`actual.txt` and `predicted.txt` are, as per the instructions, assumed to be pipe-delimited text files where each line contains one stock value in the format:
+
+```
+time|stock_name|stock_value
+```
+
+`window.txt` is assumed to be a single integer on the first line of the file.
+
+## Output
+
+The output is a file, `output/comparison.txt` which contains one line for each sliding time window, all of the form:
+
+```
+start_time|end_time|average_error
+```
+
+# Dependencies
+
+This analysis tool has a handful of dependencies, all of which are standard packages for Python scientific computing. In each case, the version of the package present on the test system is noted in parentheses. This tool was developed in Python 2.7 in a vanilla Anaconda installation.
+
+* *NumPy* (`1.15.0`)
+* *Pandas* (`0.23.4`)
+
+# Unit Tests
+
+This repository also includes all the unit tests used to prepare this submission. These tests are all contained in the `insight_testsuite/tests/` directory. This section describes each test's purpose as well as any commentary on the script's performance on the test.
+
+* `test_0`: A test consisting of the example data from the Insight-provided README.
+* `test_1`: Insight's included test of a larger sample size.
+    * The system used to test this code produced the rounding discrepencies addressed by email. `comparison.txt` was edited in this test to reflect the rounding values obtained on that system.
+
+
+# Miscellaneous Notes
---
+++ src/validation.py
@@ -0,0 +1,117 @@
+
+# coding: utf-8
+
+## TODO: Unicode input?
+## TODO: Graceful handling of read_table errors
+## TODO: Case-insensitive support?
+## TODO: Command line options
+## TODO: README
+## TODO: Check File Structure
+## TODO: Code headers
+## TODO: Unit Tests
+## TODO: Python style
+## TODO: Better directory handling
+## TODO: run.sh
+## TODO: make sure times are ordered in the output file? (they may be already)
+## TODO: FAQ 1 -- no match in time window case
+## TODO: Comment code
+## TODO: window.txt doesn't contain an integer
+## TODO: timing/general output -- take it out?
+
+import time
+start_time = time.time()
+
+import numpy as np
+import pandas as pd
+import os
+import argparse
+from itertools import tee, islice, izip
+
+#### FUNCTION DEFINITIONS ####
+
+## get a list of all time windows in a sliding window of size n
+## INPUT: a range of integers and a window size, n
+## RETURNS: a list of tuples of size n formed by rolling a window of size n over iterable
+def nwise(iterable, n=2):
+    acc = []
+    iters = tee(iterable, n)                                                     
+    for i, it in enumerate(iters):                                               
+        next(islice(it, i, i), None)
+    for tup in izip(*iters):
+        acc.append(tup)
+    return acc
+
+
+## get the average stock error for a given window
+## INPUT: a dataframe of actual & predicted values as well as a time window tuple
+## RETURNS: an average of the error for all stocks in that window, rounded to 2 digits
+def avg_window(df_actual, df_predict, win):
+    # subset the data by time window
+    actuals_in_win = df_actual.loc[df_actual['Time'].isin(win)]
+    predicts_in_win = df_predict.loc[df_predict['Time'].isin(win)]
+    
+    # a full outer join gets us NaN for missing values on either side
+    joined_wins = actuals_in_win.merge(right=predicts_in_win, 
+                                   how='outer', on=['Time', 'Stock'])
+                                   
+    # compute the average error and return
+    joined_wins['Error'] = abs(joined_wins['Value_x'] - joined_wins['Value_y'])
+    return round(joined_wins['Error'].mean(), 2)
+
+
+## utility function for formatting output with pipe delimiters
+## INPUT: a window tuple win and an error value error
+## RETURNS: A string of format "window_start|window_end|error"
+def format_result(win, error):
+    tmp = [str(win[0]), str(win[len(win)-1]), "%.2f" % error]
+    return '|'.join(tmp)
+
+
+#### FUNCTION DEFINITIONS END ####
+
+#### DIRECTORY SETUP & I/O ####
+
+# set up the directory structure
+root_dir = os.getcwd()
+print root_dir
+data_dir = os.path.join(root_dir, 'input')
+out_dir = os.path.join(root_dir, 'output')
+
+# get file locations and load
+f_actual = os.path.join(data_dir, 'actual.txt')
+f_predicted = os.path.join(data_dir, 'predicted.txt')
+f_window = os.path.join(data_dir, 'window.txt')
+f_out = os.path.join(out_dir, 'comparison.txt')
+
+actual = pd.read_table(f_actual, sep='|', header=None)
+predicted = pd.read_table(f_predicted, sep='|', header=None)
+window = int(open(f_window).readline())
+#### DIRECTORY SETUP & I/O END ####
+
+#### TIME WINDOW SETUP ####
+# relabel column headers for convenience
+actual.columns = ['Time', 'Stock', 'Value']
+predicted.columns = ['Time', 'Stock', 'Value']
+
+# calculate the times intervals needed to roll averages
+# TODO: What if the time intervals aren't the same in both files?
+t_min, t_max = actual['Time'].min(), actual['Time'].max()
+t_wins = nwise(range(t_min, t_max+1), n=window)
+
+#### TIME WINDOW SETUP END ####
+
+#### MAIN LOOP ####
+# calculate a grand average error for all windows and write to file
+with open(f_out, 'w') as out_f:
+    for t_win in t_wins:
+        print "Computing frame %s..." % str(t_win)
+        avg_err = avg_window(actual, predicted, t_win)
+        err_formatted = format_result(t_win, avg_err)
+        out_f.write(err_formatted + '\n')
+        
+#### MAIN LOOP END ####
+
+#### PRINT RESULTS ####
+print "Finished computing %s window averages from time %s to time %s." % (len(t_wins), t_min, t_max)
+print "Total time elapsed: %s seconds." % (time.time() - start_time)
+#### PRINT RESULTS END ####
