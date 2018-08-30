095fe0b351470baa809a2c5c5b07247e8fa558fb
Matt Tucker <matt.tucker@nyu.edu>
2018-08-30 11:09:06 -0400

    final commit before submission

--- src/validation.py
+++ src/validation.py
@@ -1,32 +1,53 @@
 # coding: utf-8
 
-## TODO: Code headers
-## TODO: Python style
+""" validation.py: Validate stock prediction models with rolling windows.
+Author: Matthew A. Tucker <matucker@gmail.com>
+
+This script validates stock prices from a model with actual observed
+stock prices via rolling window averages. It is a solution to the 2018
+Insight Data Engineering Fellowshio Coding Challenge.
+
+Usage: validation.py [-h] [--verbose]
+                     actuals_file predicts_file window_file output_file
+
+        Validate predictions of a stock model.
+
+        positional arguments:
+         actuals_file   The actual values file.
+         predicts_file  The predicted values file.
+         window_file    The window size file.
+         output_file    The name of the output file.
+
+        optional arguments:
+         -h, --help     show this help message and exit
+         --verbose      Print verbose command line output (default: off).
+
+--verobse is intended for debugging purposes only.
+"""
 
 import time
 import os
 import sys
 import argparse
+from itertools import tee, islice, izip
 import numpy as np
 import pandas as pd
-from itertools import tee, islice, izip
 
 start_time = time.time()
 
-
 #### FUNCTION DEFINITIONS ####
-def nwise(iterable, n=2):
+def nwise(iterable, N=2):
     """Get a list of all time windows in a sliding window.
 
     Arguments:
     iterable -- the element to form tuples over; must be iterable
-    n -- size of the rolling window (default 2)
+    N -- size of the rolling window (default 2)
 
     Returns:
     A list of tuples containing each time window to be analyzed.
     """
     acc = []
-    iters = tee(iterable, n)
+    iters = tee(iterable, N)
     for i, it in enumerate(iters):
         next(islice(it, i, i), None)
     for tup in izip(*iters):
@@ -61,8 +82,7 @@ def avg_window(df_actual, df_predict, win):
     # if there are no matching stocks in a window, return NA
     if np.isnan(err_mean):
         return 'NA'
-    else:
-        return '%.2f' % err_mean # two significant digits
+    return '%.2f' % err_mean # two significant digits
 
 
 ## utility function for formatting output with pipe delimiters
@@ -85,15 +105,17 @@ def format_result(win, error):
 #### FUNCTION DEFINITIONS END ####
 
 #### COMMAND-LINE ARGUMENTS ####
-argparser = argparse.ArgumentParser(description='Validate predictions of a stock model.')
-argparser.add_argument('actuals_file', help='The actual values file.')
-argparser.add_argument('predicts_file', help='The predicted values file.')
-argparser.add_argument('window_file', help='The window size file.')
-argparser.add_argument('output_file', help='The name of the output file.')
-argparser.add_argument('--verbose', dest='verbose', action='store_const', const=True, default=False,
-                        help='Print verbose command line output (default: off).')
+parser = argparse.ArgumentParser(description='Validate predictions of\
+                                 a stock model.')
+parser.add_argument('actuals_file', help='The actual values file.')
+parser.add_argument('predicts_file', help='The predicted values file.')
+parser.add_argument('window_file', help='The window size file.')
+parser.add_argument('output_file', help='The name of the output file.')
+parser.add_argument('--verbose', dest='verbose', action='store_const',
+                    const=True, default=False,
+                    help='Print verbose command line output (default: off).')
 
-args=argparser.parse_args()
+args=parser.parse_args()
 #### COMMAND-LINE ARGUMENTS END ####
 
 #### DIRECTORY SETUP & I/O ####
@@ -106,31 +128,39 @@ f_out = os.path.join(root_dir, args.output_file)
 
 # actually conduct the i/o
 try:
-    actual = pd.read_table(f_actual, sep='|', header=None, skipinitialspace=True, error_bad_lines=False)
+    actual = pd.read_table(f_actual, sep='|', header=None,
+                           skipinitialspace=True, error_bad_lines=False)
 except IOError as e:
-    print 'Could not read file %s.\nPlease ensure it exists and can be read.' % f_actual
+    print 'Could not read file %s.\n\
+        Please ensure it exists and can be read.' % f_actual
     sys.exit(1)
 except pd.errors.EmptyDataError as e:
-    print 'Could not parse data from file %s.\nPlease ensure it follows the specification in the README.' % f_actual
+    print 'Could not parse data from file %s.\n\
+        Please ensure it follows the specification in the README.' % f_actual
     sys.exit(1)
 
 try:
-    predicted = pd.read_table(f_predicted, sep='|', header=None, skipinitialspace=True, error_bad_lines=False)
+    predicted = pd.read_table(f_predicted, sep='|', header=None,
+                              skipinitialspace=True, error_bad_lines=False)
 except IOError as e:
-    print 'Could not read file %s.\nPlease ensure it exists and can be read.' % f_predicted
+    print 'Could not read file %s.\n\
+        Please ensure it exists and can be read.' % f_predicted
     sys.exit(1)
 except pd.errors.EmptyDataError as e:
-    print 'Could not parse data from file %s.\nPlease ensure it follows the specification in the README.' % f_predicted
+    print 'Could not parse data from file %s.\n\
+        Please ensure it follows the specification in README.md.' % f_predicted
     sys.exit(1)
 
 try:
     window = open(f_window).readline()
     window = int(window)
 except IOError as e:
-    print 'Could not read file %s.\nPlease ensure it exists and can be read.' % f_window
+    print 'Could not read file %s.\n\
+        Please ensure it exists and can be read.' % f_window
     sys.exit(1)
 except ValueError as e:
-    print 'Window.txt must contain a single integer on line one.\nCannot parse value %s.' % window
+    print 'Window.txt must contain a single integer on line one.\n\
+        Cannot parse value %s.' % window
     sys.exit(1)
 #### DIRECTORY SETUP & I/O END ####
 
@@ -184,10 +214,11 @@ if window > len(t_range):
 Using window size of %s instead.' % len(t_range)
     window = len(t_range)
 
-t_wins = nwise(t_range, n=window)
+t_wins = nwise(t_range, N=window)
 
 if args.verbose:
-    print 'Starting to compute %s window averages from time %s to time %s.' % (len(t_wins), t_min, t_max)
+    print 'Starting to compute %s window averages from time %s to time %s.'\
+        % (len(t_wins), t_min, t_max)
 #### TIME WINDOW SETUP END ####
 
 #### MAIN LOOP ####
@@ -202,13 +233,14 @@ try:
             err_formatted = format_result(t_win, avg_err)
             out_f.write(err_formatted + '\n')
 except IOError as e:
-    print 'Could not write results to file %s.\nPlease make sure this diectory exists and can be written to.' % f_out
+    print 'Could not write results to file %s.\n\
+        Please make sure this diectory exists and can be written to.' % f_out
     sys.exit(1)
-
 #### MAIN LOOP END ####
 
 #### PRINT RESULTS ####
 if args.verbose:
-    print 'Finished computing %s window averages from time %s to time %s.' % (len(t_wins), t_min, t_max)
+    print 'Finished computing %s window averages from time %s to time %s.'\
+        % (len(t_wins), t_min, t_max)
     print 'Total time elapsed: %s seconds.' % (time.time() - start_time)
 #### PRINT RESULTS END ####
